66b03cf9bd8037b0a73a8d5808e78eb9752daf9e
agarretto <agarretto@luc.edu>
2018-03-27 14:48:28 -0500

    parsing functions

--- parsing/parse.py
+++ parsing/parse.py
@@ -6,10 +6,11 @@ from itertools import islice
 log = open('/Users/andreagarretto/GeneticAncestryTool/parsing/log.txt', 'r')
 missnp = open('/Users/andreagarretto/GeneticAncestryTool/parsing/missnp.txt', 'a')
 
-missnp.write('\n')
-skipped = islice(log, 15, None) #start iterating after header
-for line in skipped:
-    id = line.split('rs',1)[1]  #gets the snp id
-    id = id.strip('\n')
-    id=id.strip("'.")
-    missnp.write('rs' +id+'\n')  #append to missnp file
+def merge_log_to_missnp(logfile,missnpfile):
+    missnpfile.write('\n')
+    skipped = islice(logfile, 15, None) #start iterating after header
+    for line in skipped:
+        id = line.split('rs',1)[1]  #gets the snp id
+        id = id.strip('\n')
+        id=id.strip("'.")
+        missnpfile.write('rs' +id+'\n')  #append to missnp file
--- parsing/parse2.py
+++ parsing/parse2.py
@@ -2,9 +2,10 @@
 import sys
 
 bim = open('/Users/andreagarretto/GeneticAncestryTool/parsing/bim.txt', 'r')
-edited_bim = open('/Users/andreagarretto/GeneticAncestryTool/parsing/edited_bim.txt', 'w')
+edited_bim = open('/Users/andreagarretto/GeneticAncestryTool/parsing/edited_bim.bim', 'w')
 
-for line in bim.readlines():
-    l = line.split('\t')  #split by tabs
-    if l[1] != '.':
-        edited_bim.write(line)
+def clean_bim(bimfile):
+    for line in bim.readlines():
+        l = line.split('\t')  #split by tabs
+        if l[1] != '.':
+            edited_bim.write(line)
