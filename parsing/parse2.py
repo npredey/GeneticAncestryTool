171b683411aba21b3f96d41aace9e0673036921a
agarretto <agarretto@luc.edu>
2018-03-27 14:55:33 -0500

    Update parse2.py

--- parsing/parse2.py
+++ parsing/parse2.py
@@ -1,9 +1,6 @@
 # remove . id's from bim file
 import sys
 
-bim = open('/Users/andreagarretto/GeneticAncestryTool/parsing/bim.txt', 'r')
-edited_bim = open('/Users/andreagarretto/GeneticAncestryTool/parsing/edited_bim.bim', 'w')
-
 def clean_bim(bimfile):
     for line in bim.readlines():
         l = line.split('\t')  #split by tabs
