122e762ab05a2875c3b1c4811c7d42c02cb325b4
nickpredey <npredey@luc.edu>
2018-04-19 23:25:26 -0500

    moved files to separate directory

--- constants/__init__.py
+++
@@ -1,7 +0,0 @@
-import os
-import sys
-# https://stackoverflow.com/questions/448271/what-is-init-py-for
-ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-sys.path.append(ROOT_DIR)
-
-sys.path.append(os.path.dirname(os.getcwd()))
